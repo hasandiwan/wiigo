0e3766cf19978ed2cfe97cf947f401d625284248
Hasan Diwan <hd1@jsc.d8u.us>
2019-11-16 23:36:57 +0100

    allow nulls

--- db/migrate/20170317005442_create_topics.rb
+++ db/migrate/20170317005442_create_topics.rb
@@ -1,14 +1,15 @@
 class CreateTopics < ActiveRecord::Migration[5.0]
   def change
     create_table :topics do |t|
-      t.string :title, null: false
-      t.string :dash_topic, null: false
-      t.text :description, null: false
-      t.string :image_url, null: false
-      t.string :banner_url, null: false
+      t.string :title
+      t.string :dash_topic
+      t.text :description
+      t.string :image_url
+      t.string :banner_url
+      t.string :search_path
       t.timestamps
-      t.index :dash_topic, unique: true
-      t.index :title, unique: true
+      t.index :dash_topic
+      t.index :title
     end
   end
 end
